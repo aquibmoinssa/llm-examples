6a4805f3c5954f24e1518de9a464f92f8703b244
aquibmoinssa <106266370+aquibmoinssa@users.noreply.github.com>
2024-03-21 12:01:14 +0000

    Formatting edits

--- Chatbot.py
+++ Chatbot.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
-    st.title("Main Menu")
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     st.subheader("Based on Streamlit LLM Examples")
